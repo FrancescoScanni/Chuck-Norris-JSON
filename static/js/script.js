6d6c6f470693a4fb763d05ce594d06c164a05d69
FrancescoScanni <frascanni07@gmail.com>
2025-02-24 00:36:51 +0100

    Chuck N (v1.2 fine + js)

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+
+//Categorie --> ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
+
+let input=document.querySelector("form")
+let joke=document.querySelector("#battuta")
+
+window.addEventListener("load",function(){
+    input.addEventListener("submit",function(e){
+        e.preventDefault()
+        categoria=document.querySelector("#menu").value
+        if(categoria!="none"){
+            let response=fetch(`https://api.chucknorris.io/jokes/random?category=${categoria}`)
+            .then(
+                function(res){
+                    return res.json()
+                }
+            ).then(
+                function(obj){
+                    console.log(obj.value)
+                    joke.innerHTML=obj.value
+                    return obj
+                }
+            ).catch(
+                function(error){
+                    console.log(error+" Ops!")
+                }
+            )
+        }
+        console.log(categoria)
+        })
+})
